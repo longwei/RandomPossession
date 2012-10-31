eb7ce0f9d8ceebf638a1e70d7281f631a1849a24
Longwei Su <longwei.su@gmail.com>
2012-10-30 23:31:11 -0400

    Initial Commit

---
+++ RandomPossession/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossession
+//
+//  Created by longwei su on 10/30/12.
+//  Copyright (c) 2012 com.longwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
